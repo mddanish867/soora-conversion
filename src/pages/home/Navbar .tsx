01f9a55eab1c28bf464f2741cedbd6db493cfee5
mddanish867 <mddanish867@gmail.com>
2024-10-15 14:38:41 +0530

    added usernavbar to the mobile navbar

--- src/pages/home/Navbar .tsx
+++ src/pages/home/Navbar .tsx
@@ -181,6 +181,52 @@ const Navbar = () => {
               </DropdownMenu>
             </div>
           </div>
+          <div className=" md:hidden flex items-center">
+            <div className="ml-4 flex items-center space-x-4">
+              <DropdownMenu>
+                <DropdownMenuTrigger asChild>
+                  <Button
+                    variant="ghost"
+                    className="relative h-8 w-8 rounded-full"
+                  >
+                    <Avatar className="h-8 w-8">
+                      <AvatarImage
+                        src="/placeholder-avatar.jpg"
+                        alt="@username"
+                      />
+                      <AvatarFallback>UN</AvatarFallback>
+                    </Avatar>
+                  </Button>
+                </DropdownMenuTrigger>
+                <DropdownMenuContent className="w-56" align="end" forceMount>
+                  <DropdownMenuLabel className="font-normal">
+                    <div className="flex flex-col space-y-1">
+                      <p className="text-sm font-medium leading-none">
+                        mddanish
+                      </p>
+                      <p className="text-xs leading-none text-muted-foreground">
+                        danish@gmail.com
+                      </p>
+                    </div>
+                  </DropdownMenuLabel>
+                  <DropdownMenuSeparator />
+                  <DropdownMenuItem onClick={handleProfile}>
+                    <User className="mr-2 h-4 w-4" />
+                    <span>Profile</span>
+                  </DropdownMenuItem>
+                  <DropdownMenuItem onClick={handleDashboard}>
+                    <LayoutDashboard className="mr-2 h-4 w-4" />
+                    <span>Dashboard</span>
+                  </DropdownMenuItem>
+                  <DropdownMenuSeparator />
+                  <DropdownMenuItem>
+                    <LogOut className="mr-2 h-4 w-4" />
+                    <span>Log out</span>
+                  </DropdownMenuItem>
+                </DropdownMenuContent>
+              </DropdownMenu>
+            </div>
+          </div>
           <div className="md:hidden flex items-center">
             <button
               onClick={() => setIsOpen(!isOpen)}
